ad5a2b1d9c9730823deca76bc5a1e4b8662e0743
1ncarnat10n <tsaiwilliam888@gmail.com>
2022-01-10 13:34:20 -0800

    Added delete function and tested

--- backend.js
+++ backend.js
@@ -36,8 +36,25 @@ const users = {
  const findUserByName = (name) => { 
     return users['users_list'].filter( (user) => user['name'] === name); 
 }
+
+function findUserById(id) {
+    return users['users_list'].find( (user) => user['id'] === id); // or line below
+    //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+function removeUser(user){
+    users['users_list'].pop(user);
+}
  
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
@@ -62,20 +79,22 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
-function findUserById(id) {
-    return users['users_list'].find( (user) => user['id'] === id); // or line below
-    //return users['users_list'].filter( (user) => user['id'] === id);
-}
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        removeUser(result);
+        res.send("user removed");
+    }
+});
 
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
     res.status(200).end();
 });
-
-function addUser(user){
-    users['users_list'].push(user);
-}
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
